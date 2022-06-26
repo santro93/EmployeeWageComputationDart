31d1beaf0d98b5bca7af6d150a3767a74eb0f07e
santro93 <nsantosh435@gmail.com>
2022-06-27 00:11:47 +0530

    Added UC6 : Calculating Wage till a condition of total working hours or days is reached for a month

--- employee_wage.dart
+++ employee_wage.dart
@@ -8,15 +8,19 @@ void main(List<String> args) {
 
 class Employee {
   void employeetype() {
-    int IS_FULL_TIME = 1;
+    final int IS_FULL_TIME = 1;
     final int IS_PART_TIME = 2;
     final int EMP_RATE_PER_HOUR = 20;
     final int NO_OF_WORKING_DAYS = 20;
     int empHours = 0;
     int empWage = 0;
     int TotalEmpWage = 0;
+    int TotalEmpHour = 0;
+    int TotalWorkingDays = 0;
 
-    for (int day = 0; day < NO_OF_WORKING_DAYS; day++) {
+    while (TotalEmpHour <= 100 && TotalWorkingDays <= NO_OF_WORKING_DAYS) {
+      TotalWorkingDays++;
+      // for (int day = 0; day < NO_OF_WORKING_DAYS; day++) {
       int empCheck = Random().nextInt(3);
       switch (empCheck) {
         case (1):
@@ -33,11 +37,15 @@ class Employee {
 
         default:
           print("Employee is Absent.");
+          empHours = 0;
+          empWage = empHours * EMP_RATE_PER_HOUR;
       }
       empWage = empHours * EMP_RATE_PER_HOUR;
       TotalEmpWage += empWage;
+      TotalEmpHour += empHours;
       print("Employee Wage: $empWage");
     }
+    print("Total Employee Wage : $TotalEmpWage");
     print("Total Employee Wage : $TotalEmpWage");
   }
 }
