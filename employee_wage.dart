afdfc83c627280b91b58058be69e1fdcab69b66e
santro93 <nsantosh435@gmail.com>
2022-06-26 20:53:32 +0530

    Added UC2 : Calculated Daily Wage of Employee

--- employee_wage.dart
+++ employee_wage.dart
@@ -9,12 +9,20 @@ void main(List<String> args) {
 class Employee {
   void employeetype() {
     int IS_FULL_TIME = 1;
+    int EMP_RATE_PER_HOUR = 20;
+    int empHours = 0;
+    int empWage = 0;
+
     int empCheck = Random().nextInt(2);
 
     if (empCheck == IS_FULL_TIME) {
       print("Employee is Present");
+      empHours = 8;
+      empWage = empHours * EMP_RATE_PER_HOUR;
+      print("Employee Wage: $empWage");
     } else {
       print("Employee is Absent");
+      print("Employee Wage: $empWage");
     }
   }
 }
