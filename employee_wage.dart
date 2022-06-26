5e9ac9a1d94cdd3ea127dbbf6c5420146a6425c7
santro93 <nsantosh435@gmail.com>
2022-06-26 20:46:53 +0530

    Added UC1 : Check Employee is Present or Absent.

--- employee_wage.dart
+++ employee_wage.dart
@@ -1,4 +1,20 @@
+import 'dart:math';
 
 void main(List<String> args) {
   print("Welcome to Employee Wage Computation Program!");
+  Employee employee = new Employee();
+  employee.employeetype();
+}
+
+class Employee {
+  void employeetype() {
+    int IS_FULL_TIME = 1;
+    int empCheck = Random().nextInt(2);
+
+    if (empCheck == IS_FULL_TIME) {
+      print("Employee is Present");
+    } else {
+      print("Employee is Absent");
+    }
+  }
 }
