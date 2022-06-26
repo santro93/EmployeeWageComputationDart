c76a3f93c9315398ba22337d8dab66adc282137f
santro93 <nsantosh435@gmail.com>
2022-06-26 22:37:27 +0530

    Added UC5 : Calculating Wage for a Month.

--- employee_wage.dart
+++ employee_wage.dart
@@ -9,29 +9,38 @@ void main(List<String> args) {
 class Employee {
   void employeetype() {
     int IS_FULL_TIME = 1;
-    int IS_PART_TIME = 2;
-    int EMP_RATE_PER_HOUR = 20;
+    final int IS_PART_TIME = 2;
+    final int EMP_RATE_PER_HOUR = 20;
+    final int NO_OF_WORKING_DAYS = 20;
     int empHours = 0;
     int empWage = 0;
+    int TotalEmpWage = 0;
 
-    int empCheck = Random().nextInt(3);
-    switch (empCheck) {
-      case (1):
-        print("Employee is Present Full Time.");
-        empHours = 8;
-        empWage = empHours * EMP_RATE_PER_HOUR;
-        print("Present Employee Wage: $empWage");
-        break;
+    for (int day = 0; day < NO_OF_WORKING_DAYS; day++) {
+      int empCheck = Random().nextInt(3);
+      switch (empCheck) {
+        case (1):
+          print("Employee is Present Full Time.");
+          empHours = 8;
+          empWage = empHours * EMP_RATE_PER_HOUR;
+          print("Present Employee Wage: $empWage");
+          break;
 
-      case (2):
-        print("Employee is Present Part Time.");
-        empHours = 4;
-        empWage = empHours * EMP_RATE_PER_HOUR;
-        print("Part Time Employee Wage: $empWage");
-        break;
-      default:
-        print("Employee is Absent.");
-        print("Absent Employee Wage: $empWage");
+        case (2):
+          print("Employee is Present Part Time.");
+          empHours = 4;
+          empWage = empHours * EMP_RATE_PER_HOUR;
+          print("Part Time Employee Wage: $empWage");
+          break;
+          
+        default:
+          print("Employee is Absent.");
+          print("Absent Employee Wage: $empWage");
+      }
+      empWage = empHours * EMP_RATE_PER_HOUR;
+      TotalEmpWage += empWage;
+      print("Employee Wage: $empWage");
     }
+    print("Total Employee Wage : $TotalEmpWage");
   }
 }
